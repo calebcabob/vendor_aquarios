717ec67b1827e1c6a02eab937c24717ceca93808
calebcabob <calphonic@gmail.com>
2018-11-19 17:44:52 -0500

    Taimen/Walleye: Update fingerprint to Nov 2018
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- products/walleye.mk
+++ products/walleye.mk
@@ -31,6 +31,6 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    PRIVATE_BUILD_DESC="walleye-user 9 PPR2.181005.003 4984323 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 9 PQ1A.181105.017.A1 5081125 release-keys"
 
-BUILD_FINGERPRINT := google/walleye/walleye:9/PPR2.181005.003/4984323:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:9/PQ1A.181105.017.A1/5081125:user/release-keys
