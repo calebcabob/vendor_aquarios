AOSiP/platform_vendor_aosip

repopick: support picking commits from GitHub instead of Gerrit
This partially reverts commit f5fd8d822b9385b286987e8c90af9195fc81fd58.

Change-Id: I861ad8abde42ff6d4287bf9cfc7f8943fb83c4ed
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

build/tools/repopick.py
@@ -363,22 +363,45 @@ if __name__ == '__main__':
         else:
             method = 'ssh'
 
-        if args.verbose:
-            print('Fetching from {0}'.format(args.gerrit))
+        # Try fetching from GitHub first if using default gerrit
+        if args.gerrit == default_gerrit:
+            if args.verbose:
+                print('Trying to fetch the change from GitHub')
 
-        if args.pull:
-            cmd = ['git pull --no-edit', item['fetch'][method]['url'], item['fetch'][method]['ref']]
-        else:
-            cmd = ['git fetch', item['fetch'][method]['url'], item['fetch'][method]['ref']]
-        if args.quiet:
-            cmd.append('--quiet')
-        else:
-            print(cmd)
-        result = subprocess.call([' '.join(cmd)], cwd=project_path, shell=True)
-        if result != 0:
-            print('ERROR: git command failed')
-            sys.exit(result)
+            if args.pull:
+                cmd = ['git pull --no-edit github', item['fetch'][method]['ref']]
+            else:
+                cmd = ['git fetch github', item['fetch'][method]['ref']]
+            if args.quiet:
+                cmd.append('--quiet')
+            else:
+                print(cmd)
+            result = subprocess.call([' '.join(cmd)], cwd=project_path, shell=True)
+            FETCH_HEAD = '{0}/.git/FETCH_HEAD'.format(project_path)
+            if result != 0 and os.stat(FETCH_HEAD).st_size != 0:
+                print('ERROR: git command failed')
+                sys.exit(result)
+        # Check if it worked
+        if args.gerrit != default_gerrit or os.stat(FETCH_HEAD).st_size == 0:
+            # If not using the default gerrit or github failed, fetch from gerrit.
+            if args.verbose:
+                if args.gerrit == default_gerrit:
+                    print('Fetching from GitHub didn\'t work, trying to fetch the change from Gerrit')
+                else:
+                    print('Fetching from {0}'.format(args.gerrit))
 
+            if args.pull:
+                cmd = ['git pull --no-edit', item['fetch'][method]['url'], item['fetch'][method]['ref']]
+            else:
+                cmd = ['git fetch', item['fetch'][method]['url'], item['fetch'][method]['ref']]
+            if args.quiet:
+                cmd.append('--quiet')
+            else:
+                print(cmd)
+            result = subprocess.call([' '.join(cmd)], cwd=project_path, shell=True)
+            if result != 0:
+                print('ERROR: git command failed')
+                sys.exit(result)
         # Perform the cherry-pick
         if not args.pull:
             cmd = ['git cherry-pick FETCH_HEAD']

calebcabob/vendor_aquarios

tools/repopick.py
@@ -363,22 +363,45 @@ if __name__ == '__main__':
         else:
             method = 'ssh'
 
-        if args.verbose:
-            print('Fetching from {0}'.format(args.gerrit))
+        # Try fetching from GitHub first if using default gerrit
+        if args.gerrit == default_gerrit:
+            if args.verbose:
+                print('Trying to fetch the change from GitHub')
 
-        if args.pull:
-            cmd = ['git pull --no-edit', item['fetch'][method]['url'], item['fetch'][method]['ref']]
-        else:
-            cmd = ['git fetch', item['fetch'][method]['url'], item['fetch'][method]['ref']]
-        if args.quiet:
-            cmd.append('--quiet')
-        else:
-            print(cmd)
-        result = subprocess.call([' '.join(cmd)], cwd=project_path, shell=True)
-        if result != 0:
-            print('ERROR: git command failed')
-            sys.exit(result)
+            if args.pull:
+                cmd = ['git pull --no-edit aqua', item['fetch'][method]['ref']]
+            else:
+                cmd = ['git fetch aqua', item['fetch'][method]['ref']]
+            if args.quiet:
+                cmd.append('--quiet')
+            else:
+                print(cmd)
+            result = subprocess.call([' '.join(cmd)], cwd=project_path, shell=True)
+            FETCH_HEAD = '{0}/.git/FETCH_HEAD'.format(project_path)
+            if result != 0 and os.stat(FETCH_HEAD).st_size != 0:
+                print('ERROR: git command failed')
+                sys.exit(result)
+        # Check if it worked
+        if args.gerrit != default_gerrit or os.stat(FETCH_HEAD).st_size == 0:
+            # If not using the default gerrit or github failed, fetch from gerrit.
+            if args.verbose:
+                if args.gerrit == default_gerrit:
+                    print('Fetching from GitHub didn\'t work, trying to fetch the change from Gerrit')
+                else:
+                    print('Fetching from {0}'.format(args.gerrit))
 
+            if args.pull:
+                cmd = ['git pull --no-edit', item['fetch'][method]['url'], item['fetch'][method]['ref']]
+            else:
+                cmd = ['git fetch', item['fetch'][method]['url'], item['fetch'][method]['ref']]
+            if args.quiet:
+                cmd.append('--quiet')
+            else:
+                print(cmd)
+            result = subprocess.call([' '.join(cmd)], cwd=project_path, shell=True)
+            if result != 0:
+                print('ERROR: git command failed')
+                sys.exit(result)
         # Perform the cherry-pick
         if not args.pull:
             cmd = ['git cherry-pick FETCH_HEAD']